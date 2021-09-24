f3f8a483485274074decd18c246661dcc2fa8e29
RoVRy <58503026+RoVRy@users.noreply.github.com>
2021-09-24 23:56:55 +0300

    Add files via upload
    1. Fixed MS page parsing.
    2. Add additional texts.

--- Download.ps1
+++ Download.ps1
@@ -1,9 +1,9 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-
 function ShowErrorNExit($ErrorCode) {
-    Write-Host $ErrorText[$LngIndex]' '$ErrorCode'`n' -ForegroundColor Red
-    Write-Host $PaKText[$LngIndex]'`n'
+    Write-Host $ErrorText[$LngIndex] -ForegroundColor Red -NoNewline
+    " 0x{0:X} ({0:D})`n" -f $ErrorCode
+    Write-Host $PaKText[$LngIndex]"`n"
     [void] $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,AllowCtrlC")
 }
 
@@ -67,6 +67,20 @@ $PaKText = @("Press any key...",
              "Appuyez sur une touche...",
              "Stiskněte libovolnou klávesu...",
              "どれかキーを押してください...")
+$VersionText = @("PowerShell version must be 7.0 and higher",
+                 "La versión de PowerShell debe ser 7.0 y superior",
+                 "La versione di PowerShell deve essere 7.0 e successive",
+                 "PowerShell 版本必须是 7.0 及更高版本",
+                 "PowerShell 版本必須是 7.0 及更高版本",
+                 "PowerShell 버전은 7.0 이상이어야 합니다.",
+                 "PowerShell-Version muss 7.0 und höher sein",
+                 "Wersja PowerShell musi być 7.0 lub nowsza",
+                 "A versão do PowerShell deve ser 7.0 e superior",
+                 "Версия PowerShell должна быть 7.0 и выше",
+                 "PowerShell sürümü 7.0 ve üzeri olmalıdır",
+                 "La version de PowerShell doit être 7.0 et supérieure",
+                 "Verze prostředí PowerShell musí být 7.0 a vyšší",
+                 "PowerShellのバージョンは7.0以降である必要があります")
 $ConsoleText1 = @("Selected Visual Studio Edition: ",
                   "Edición de Visual Studio seleccionada: ",
                   "Edizione di Visual Studio selezionata: ",
@@ -151,20 +165,43 @@ $ConsoleText6 = @("Old packages removed.`nVisual Studio update completed.",
                   "Anciens packages supprimés.`nMise à jour de Visual Studio terminée.",
                   "Staré balíčky odstraněny.`nAktualizace sady Visual Studio dokončena.",
                   "古いパッケージが削除されました。`nVisual Studio の更新が完了しました。")
+$ConsoleText7 = @("No archives were found for the cleanup. Exiting immediately.",
+                  "No se encontraron archivos para la limpieza. Saliendo de inmediato.",
+                  "Nessun archivio trovato per la pulizia. Uscita immediata.",
+                  "没有找到用于清理的档案。 立即退出。",
+                  "沒有找到用於清理的檔案。 立即退出。",
+                  "정리에 대한 아카이브를 찾을 수 없습니다. 즉시 종료합니다.",
+                  "Für die Bereinigung wurden keine Archive gefunden. Ausstieg sofort.",
+                  "Nie znaleziono archiwów do czyszczenia. Wyjście natychmiast.",
+                  "Nenhum arquivo foi encontrado para a limpeza. Saindo imediatamente.",
+                  "Архивы для очистки не найдены. Завершаем работу.",
+                  "Temizleme için arşiv bulunamadı. Hemen çıkış.",
+                  "Aucune archive n'a été trouvée pour le nettoyage. Sortie immédiate.",
+                  "Pro vyčištění nebyly nalezeny žádné archivy. Okamžitý odchod.",
+                  "クリーンアップ用のアーカイブは見つかりませんでした。 すぐに終了します。")
 
+# Get all languages and filter with our list
 $Cult = Get-Culture -ListAvailable
-$LngName = Get-Culture
-$LngIndex = $Languages[0].IndexOf($LngName.Name)
+$LngName = $(Get-UICulture).Name
+$LngIndex = $Languages[0].IndexOf($LngName)
 if ($LngIndex -eq -1) {
     $LngIndex = 0
 }
+# Get the language readable name from the short form
 foreach ($item in $Languages[0]) {
     $result = $Cult | Where-Object { $_.Name -like $item }
     $Languages[1] += (Get-Culture).TextInfo.ToTitleCase($result.DisplayName)
 }
 
+if ($Host.Version.Major -lt 7) {
+    Write-Host $VersionText[$LngIndex]
+    Exit
+}
+
+$ProgressPreference = 'SilentlyContinue'
+# Get the web page with download links, take all unique URLs by mask
 $Editions = @()
-$AllURLs = Invoke-WebRequest "https://visualstudio.microsoft.com/${$LngName.Name}/downloads/" | Select-String -InputObject { $_.Content } -Pattern 'http\S+studio/\?sku\S+(?=\")' -AllMatches
+$AllURLs = Invoke-WebRequest "https://visualstudio.microsoft.com/$LngName/downloads/" | Select-String -InputObject { $_.Content } -Pattern 'http\S+studio\/\?sku\S+(?=\")' -AllMatches
 $UniqURLs = $AllURLs.Matches.Value | Select-Object -Unique
 foreach ($item in $UniqURLs) {
     $result = Select-String -InputObject $item -Pattern '(?<=sku\=)\w+'
@@ -174,7 +211,7 @@ foreach ($item in $UniqURLs) {
 -------------------- Window form --------------------
 #>
 $form = New-Object System.Windows.Forms.Form
-$form.Text = $WindowTitle[$LngIndex]
+$form.Text = $WindowTitle[$LngIndex]                                        # Offline package settings
 $form.Size = New-Object System.Drawing.Size(400,370)
 $form.FormBorderStyle = 'FixedSingle'
 $form.MaximizeBox = $false
@@ -183,7 +220,7 @@ $form.StartPosition = 'CenterScreen'
 $label1 = New-Object System.Windows.Forms.Label
 $label1.Location = New-Object System.Drawing.Point(20, 20)
 $label1.Size = New-Object System.Drawing.Size(150, 20)
-$label1.Text = $EditionTitle[$LngIndex]
+$label1.Text = $EditionTitle[$LngIndex]                                     # Visual Studio Edition
 $form.Controls.Add($label1)
 
 $listboxEd = New-Object System.Windows.Forms.ComboBox
@@ -199,7 +236,7 @@ $form.Controls.Add($listboxEd)
 $label2 = New-Object System.Windows.Forms.Label
 $label2.Location = New-Object System.Drawing.Point(20, 50)
 $label2.Size = New-Object System.Drawing.Size(250, 20)
-$label2.Text = $LangTitle[$LngIndex]
+$label2.Text = $LangTitle[$LngIndex]                                        # Needed languages
 $form.Controls.Add($label2)
 
 $listboxLng = New-Object System.Windows.Forms.ListBox
@@ -216,7 +253,7 @@ $form.Controls.Add($listboxLng)
 $okButton = New-Object System.Windows.Forms.Button
 $okButton.Location = New-Object System.Drawing.Point(260, 240)
 $okButton.Size = New-Object System.Drawing.Size(100, 30)
-$okButton.Text = $OKText[$LngIndex]
+$okButton.Text = $OKText[$LngIndex]                                         # OK button
 $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $okButton
 $form.Controls.Add($okButton)
@@ -224,7 +261,7 @@ $form.Controls.Add($okButton)
 $cancelButton = New-Object System.Windows.Forms.Button
 $cancelButton.Location = New-Object System.Drawing.Point(260, 280)
 $cancelButton.Size = New-Object System.Drawing.Size(100, 30)
-$cancelButton.Text = $CancelText[$LngIndex]
+$cancelButton.Text = $CancelText[$LngIndex]                                 # Cancel button
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 $form.CancelButton = $cancelButton
 $form.Controls.Add($cancelButton)
@@ -238,7 +275,7 @@ if ($result -ne $OKText[$LngIndex]) {
 -------------------- Main code --------------------
 #>
 $Edition = $Editions[$listboxEd.SelectedIndex]
-$Exe = ".\$Edition\vs_$Edition.exe"
+$Exe = ".\vs_$Edition.exe"
 $DownloadLink = $UniqURLs[$listboxEd.SelectedIndex]
 $Prm = "--all --layout .\$Edition"
 $PrmLng = " --lang"
@@ -252,9 +289,9 @@ if ($listboxLng.SelectedItems.Count -ne 14) {
     $Prm += $PrmLng
 }
 
-Write-Host $ConsoleText1[$LngIndex] -ForegroundColor Blue -NoNewline
+Write-Host $ConsoleText1[$LngIndex] -ForegroundColor Cyan -NoNewline        # Selected Visual Studio Edition
 Write-Host $Edition -ForegroundColor White
-Write-Host $ConsoleText2[$LngIndex] -ForegroundColor Blue -NoNewline
+Write-Host $ConsoleText2[$LngIndex] -ForegroundColor Cyan -NoNewline        # Getting a link to the online installer
 
 $result = Invoke-WebRequest $DownloadLink
 if ($result.StatusCode -ne 200) {
@@ -269,7 +306,7 @@ if ($URL.Matches.Lenght -eq 0) {
 }
 
 Write-Host "${$URL.Matches.Value}`n"
-Write-Host $ConsoleText3[$LngIndex] -ForegroundColor Blue -NoNewline
+Write-Host $ConsoleText3[$LngIndex] -ForegroundColor Cyan -NoNewline        # Downloading online-installer
 $result = Invoke-WebRequest $URL.Matches.Value -OutFile "installer.tmp"
 Write-Host $OKText[$LngIndex]"`n" -ForegroundColor Green
 
@@ -291,7 +328,7 @@ else {
         Remove-Item -Path "installer.tmp"
     }
 }
-Write-Host $ConsoleText4[$LngIndex] -ForegroundColor Blue -NoNewline
+Write-Host $ConsoleText4[$LngIndex] -ForegroundColor Cyan -NoNewline        # Launch the online installer in the offline catalog creation/update mode
 
 $Process = Start-Process -FilePath $Exe -ArgumentList $Prm -Wait -PassThru
 if ($Process.ExitCode -ne 0) {
@@ -299,18 +336,24 @@ if ($Process.ExitCode -ne 0) {
     exit
 }
 Write-Host $OKText[$LngIndex]"`n" -ForegroundColor Green
+
+Write-Host $ConsoleText5[$LngIndex] -ForegroundColor Cyan -NoNewline        # Running the online installer in the cleanup mode from the obsolete packages
 $Arch = ".\$Edition\Archive"
 if ((Test-Path -Path $Arch) -eq $True) {
     $Cln = ""
     Get-ChildItem -Path $Arch -Directory | ForEach-Object $Cln = (($Cln -eq "") ? "" : $Cln + " ") + "--clean .\Archive\$_\Catalog.json"
-    Write-Host $ConsoleText5[$LngIndex] -ForegroundColor Blue -NoNewline
     $Process = Start-Process -FilePath $Exe -ArgumentList $Prm $Cln -Wait -PassThru
     if ($Process.ExitCode -ne 0) {
         ShowErrorNExit($Process.ExitCode)
         exit
     }
-    Write-Host $OKText[$LngIndex]"`n" -ForegroundColor Green
+    Write-Host -ForegroundColor Green $OKText[$LngIndex]"`n"
     Remove-Item -Path $Arch -Recurse
-    Write-Host $ConsoleText6[$LngIndex]"`n" -ForegroundColor Blue
+    Write-Host -ForegroundColor Cyan $ConsoleText6[$LngIndex]"`n"           # Old packages removed
     exit
 }
+else {
+    Write-Host $ConsoleText7[$LngIndex] -ForegroundColor Green              # No archive founded
+}
+Remove-Item -Path $Exe
+$ProgressPreference = 'Continue'
